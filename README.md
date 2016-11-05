bfa43d30fc3af1db5eeb6448eb97b4ae41246a5d
Jin Qing <jinq0123@163.com>
2016-11-05 13:50:11 +0800

    Change ClientSyncWriter.

--- README.md
+++ README.md
@@ -7,3 +7,18 @@ C++ [gRPC](http://www.grpc.io/) library with callback interface.
 1. Copy [grpc](https://github.com/grpc/grpc) as third-party/grpc, and build grpc.
 2. Use build/grpc_cb.sln or build/Makefile to build grpc_cb.
 
+## Todo
+
+* ClientSyncReader 				DONE
+* ClientSyncReaderHelper		DONE
+* ClientSyncWriter				DONE
+* ClientSyncWriterHelper
+* ClientSyncReaderWriter
+* ClientSyncReaderWriterImpl
+* ClientAsyncReader
+* ClientAsyncReaderHelper
+* ClientAsyncWriter
+* ClientAsyncWriterHelper
+* ClientAsyncReaderWriter
+* ClientAsyncReaderWriterImpl
+
--- include/grpc_cb/client_sync_writer.h
+++ include/grpc_cb/client_sync_writer.h
@@ -23,7 +23,6 @@ template <class Request>
 class ClientSyncWriter GRPC_FINAL {
  public:
   inline ClientSyncWriter(const ChannelSptr& channel, const std::string& method);
-                      // XXX const CompletionQueueSptr& cq_sptr);
 
   // Todo: BlockingGetInitMd();
   bool Write(const Request& request) const {
@@ -31,10 +30,7 @@ class ClientSyncWriter GRPC_FINAL {
     return ClientSyncWriterHelper::BlockingWrite(d.call_sptr, request, d.status);
   }
 
-  Status Finish(
-      ::google::protobuf::Message* response) const;
-
-  // Todo: AsyncFinish
+  Status Finish(::google::protobuf::Message* response) const;
 
  private:
   // Wrap all data in shared struct pointer to make copy quick.
@@ -50,7 +46,7 @@ class ClientSyncWriter GRPC_FINAL {
 
 template <class Request>
 ClientSyncWriter<Request>::ClientSyncWriter(const ChannelSptr& channel,
-                                    const std::string& method)
+                                            const std::string& method)
     // Todo: same as ClientReader?
     : data_sptr_(new Data) {
   assert(channel);
@@ -58,10 +54,13 @@ ClientSyncWriter<Request>::ClientSyncWriter(const ChannelSptr& channel,
   CallSptr call_sptr = channel->MakeSharedCall(method, *cq_sptr);
   data_sptr_->cq_sptr = cq_sptr;
   data_sptr_->call_sptr = call_sptr;
-  ClientInitMdCqTag* tag = new ClientInitMdCqTag(call_sptr);
-  if (tag->Start()) return;
-  delete tag;
-  data_sptr_->status.SetInternalError("Failed to init client stream.");
+  ClientInitMdCqTag tag(call_sptr);
+  if (tag.Start()) {
+    cq_sptr->Pluck(&tag);
+    return;
+  }
+
+  data_sptr_->status.SetInternalError("Failed to start client sync writer.");
 }
 
 template <class Request>
