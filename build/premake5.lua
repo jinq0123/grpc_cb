d98a8a6d1530f4a5062dba4b49819c25e171643f
Jin Qing <jinq0123@163.com>
2017-02-19 14:31:38 +0800

    Delete grpc_cb shared lib.

--- build/premake5.lua
+++ build/premake5.lua
@@ -76,28 +76,8 @@ project "grpc_cpp_cb_plugin"
 		}
 	filter {}
 
-project "grpc_cb_shared"
-	kind "SharedLib"
-	targetname "grpc_cb"
-	includedirs {
-		"../src/cpp_cb",
-	}
-	files {
-		"../include/grpc_cb/**.h",
-		"../src/cpp_cb/**",
-	}
-	filter { "system:windows" }
-		links { "grpc_dll" }
-	filter { "system:not windows"}
-		links { "grpc" }
-	filter {}
-
-project "grpc_cb_static"
+project "grpc_cb"
 	kind "StaticLib"
-	targetname "grpc_cb"
-	filter "system:windows"
-		targetprefix "lib"  -- libgrpc_cb.lib
-	filter {}
 	includedirs {
 		"../src/cpp_cb",
 	}
@@ -109,7 +89,7 @@ project "grpc_cb_static"
 group "examples"
 
 	examples_dep_libs = {
-		"grpc_cb_static",
+		"grpc_cb",
 		"grpc",
 		"gpr",
 		"zlib",
